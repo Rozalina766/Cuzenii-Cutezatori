9dc84693ab0fc60fa0e6cddb5fb8e73a3cb81e1b
Rozalina766 <mosiencorozalina59@gmail.com>
2025-03-21 13:16:31 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+AOS.init({
+    duration: 500,
+    once: true,
+    offset: 100
+});
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+var form = document.getElementById('formular');
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method: "POST",
+        body: new FormData(document.getElementById("formular")),
+    }).then(() => {
+        alert("Multumesc !");
+        form.reset();
+    });
+});
+
